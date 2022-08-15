6b362faf6afe948f735e4acb786ff2d7ddcc532a
Stark <serj.elessar@gmail.com>
2022-08-15 22:57:27 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-const { json } = require('express')
 const Router = require('express')
 const router = new Router
 const ctrlTelegram = require('../api/telegramMsg')
 
 router.post('/telegram', ctrlTelegram.sendMessage)
-router.get('/', (req, res) => {
-  return res.json({ message: 'ok' })
-})
+router.get('/', (req, res) => res.json({ message: 'ok' }))
 
 module.exports = router
